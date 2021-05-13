ca4025e956283a3285a852a0418deb3d3392e882
Md. Saddam Hossain <saddambubt65@gmail.com>
2021-05-13 11:51:45 +0600

    CRUD operation done in row nodejs

--- index.js
+++ index.js
@@ -9,20 +9,23 @@
 // dependencies
 const http = require('http');
 const { handleReqRes } = require('./helpers/handleReqRes');
+const environment = require('./helpers/environments');
+const data = require('./lib/data');
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-    port: 3000,
-};
+// testing file system
+// @TODO pore muche dibo
+data.delete('test', 'newFile', (err) => {
+    console.log(err);
+});
 
 // create server
 app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`listening to port ${app.config.port}`);
+    server.listen(environment.port, () => {
+        console.log(`listening to port ${environment.port}`);
     });
 };
 
